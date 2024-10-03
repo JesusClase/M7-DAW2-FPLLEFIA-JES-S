a5c8606fefc55707c5d8a3a0de38038b0f041f71
JesusClase <156935177+JesusClase@users.noreply.github.com>
2024-10-03 18:48:50 +0000

    Guardado 3/10

--- practics/pac3-multidimensional/ejercicio3b.php
+++ practics/pac3-multidimensional/ejercicio3b.php
@@ -2,11 +2,11 @@
 <?php
 
 $frutas = [
-    ["fruta" => "manzana", "eleccion" => "Esta es tu fruta favorita", "foto" => "https://static.vecteezy.com/system/resources/previews/023/858/853/non_2x/ai-genrative-green-apple-free-png.png"],
-    ["fruta" => "platano", "eleccion" => "Esta es tu fruta favorita", "foto" => "https://static.vecteezy.com/system/resources/previews/012/909/741/original/single-of-banana-png.png"],
-    ["fruta" => "naranja", "eleccion" => "Esta es tu fruta favorita", "foto" => "https://static.vecteezy.com/system/resources/thumbnails/040/750/078/small_2x/ai-generated-orange-orange-isolated-refreshing-citrus-fruit-orange-slices-orange-top-view-orange-flat-lay-png.png"],
-    ["fruta" => "fresa", "eleccion" => "Esta es tu fruta favorita", "foto" => "https://static.vecteezy.com/system/resources/previews/024/596/055/non_2x/strawberry-strawberry-with-transparent-background-transparent-strawberry-ai-generated-free-png.png"],
-    ["fruta" => "kiwi", "eleccion" => "Esta es tu fruta favorita", "foto" => "https://static.vecteezy.com/system/resources/previews/008/532/603/non_2x/kiwi-fruit-cutout-file-png.png"],
+    ["fruta" => "Manzana", "tf" => false, "foto" => "https://static.vecteezy.com/system/resources/previews/023/858/853/non_2x/ai-genrative-green-apple-free-png.png"],
+    ["fruta" => "Platano", "tf" => false, "foto" => "https://static.vecteezy.com/system/resources/previews/012/909/741/original/single-of-banana-png.png"],
+    ["fruta" => "Naranja", "tf" => false, "foto" => "https://static.vecteezy.com/system/resources/thumbnails/040/750/078/small_2x/ai-generated-orange-orange-isolated-refreshing-citrus-fruit-orange-slices-orange-top-view-orange-flat-lay-png.png"],
+    ["fruta" => "Fresa", "tf" => false, "foto" => "https://static.vecteezy.com/system/resources/previews/024/596/055/non_2x/strawberry-strawberry-with-transparent-background-transparent-strawberry-ai-generated-free-png.png"],
+    ["fruta" => "Kiwi", "tf" => false, "foto" => "https://static.vecteezy.com/system/resources/previews/008/532/603/non_2x/kiwi-fruit-cutout-file-png.png"],
 ]
 ?>
 
@@ -35,11 +35,30 @@ $frutas = [
                 foreach ($frutas as $fruta){
                     echo'
                     <tr class="table-danger">;
-                    <td>Manzana</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="ejercicio3b.php?">Seleccionar</a></td>
+                    <td>' .$fruta['fruta'] . '</td>';
+                    
+                    if($fruta['tf']==false){
+                        echo '<td>No seleccionada</td>';
+                        $fruta['tf'] = true;
+                        if(isset($_GET['tf']) == true){
+                            echo '<td> ✔️ Fruta seleccionada</td>';
+                        }
+                    }
+                    echo'
+                    <td><a class="btn btn-primary" href="?fruta='.$fruta['fruta'] .'">Seleccionar</a></td>
                 </tr>';
                 }
+
+                /*if (isset($_GET['fruta'])){
+                    
+                    echo'
+                    <tr class="table-danger">;
+                    <td>' .$fruta['fruta'] . '</td>
+                    <td>✔️Fruta seleccionada</td>
+                    <td><a class="btn btn-primary" href="?fruta='.$fruta['fruta'] .'">Seleccionar</a></td>
+                </tr>';
+
+                }*/
                 ?>
             </tbody>
         </table>
